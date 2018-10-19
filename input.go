1dcc4886ee044e2a1b0055384890ffa7fad2e76c
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-10-19 11:33:50 +0100

    Mouse inputs added

--- input.go
+++ input.go
@@ -8,45 +8,53 @@ import (
 )
 
 func mouseCallback(w *glfw.Window, xpos, ypos float64) {
-	if firstMouse {
-		globalCamera.LastPos = [2]float64{xpos, ypos}
-		firstMouse = false
+	if FIRSTMOUSE {
+		GLOBALCAMERA.LastPos = [2]float64{xpos, ypos}
+		FIRSTMOUSE = false
 	}
 
-	xoffset, yoffset := xpos - globalCamera.LastPos[0], globalCamera.LastPos[1] - ypos
-	globalCamera.LastPos[0], globalCamera.LastPos[1] = xpos, ypos
+	xoffset, yoffset := xpos-GLOBALCAMERA.LastPos[0], GLOBALCAMERA.LastPos[1]-ypos
+	GLOBALCAMERA.LastPos[0], GLOBALCAMERA.LastPos[1] = xpos, ypos
 
 	sensitivity := 0.1
 	xoffset *= sensitivity
 	yoffset *= sensitivity
 
-	globalCamera.Yaw += xoffset
-	globalCamera.Yaw = math.Mod(globalCamera.Yaw, 360)
-	if globalCamera.Pitch += yoffset; globalCamera.Pitch > 89.0 {
-		globalCamera.Pitch = 89.0
-	}else if globalCamera.Pitch < -89.0 {
-		globalCamera.Pitch = -89.0
+	GLOBALCAMERA.Yaw += xoffset
+	GLOBALCAMERA.Yaw = math.Mod(GLOBALCAMERA.Yaw, 360)
+	if GLOBALCAMERA.Pitch += yoffset; GLOBALCAMERA.Pitch > 89.0 {
+		GLOBALCAMERA.Pitch = 89.0
+	} else if GLOBALCAMERA.Pitch < -89.0 {
+		GLOBALCAMERA.Pitch = -89.0
 	}
 
-	globalCamera.CamFront = mgl32.Vec3{
-		float32(math.Cos(mgl64.DegToRad(globalCamera.Pitch)) * math.Cos(mgl64.DegToRad(globalCamera.Yaw))),
-		float32(math.Sin(mgl64.DegToRad(globalCamera.Pitch))),
-		float32(math.Cos(mgl64.DegToRad(globalCamera.Pitch)) * math.Sin(mgl64.DegToRad(globalCamera.Yaw))),
+	GLOBALCAMERA.CamFront = mgl32.Vec3{
+		float32(math.Cos(mgl64.DegToRad(GLOBALCAMERA.Pitch)) * math.Cos(mgl64.DegToRad(GLOBALCAMERA.Yaw))),
+		float32(math.Sin(mgl64.DegToRad(GLOBALCAMERA.Pitch))),
+		float32(math.Cos(mgl64.DegToRad(GLOBALCAMERA.Pitch)) * math.Sin(mgl64.DegToRad(GLOBALCAMERA.Yaw))),
 	}.Normalize()
 }
 
 func processInput(window *glfw.Window) {
-	camSpeed := float32(20 * deltaTime)
+	camSpeed := float32(20 * DELTATIME)
 	if window.GetKey(glfw.KeyW) == glfw.Press {
-		globalCamera.CamPosition = globalCamera.CamPosition.Add(globalCamera.CamFront.Mul(camSpeed))
+		GLOBALCAMERA.CamPosition = GLOBALCAMERA.CamPosition.Add(GLOBALCAMERA.CamFront.Mul(camSpeed))
 	}
 	if window.GetKey(glfw.KeyS) == glfw.Press {
-		globalCamera.CamPosition = globalCamera.CamPosition.Sub(globalCamera.CamFront.Mul(camSpeed))
+		GLOBALCAMERA.CamPosition = GLOBALCAMERA.CamPosition.Sub(GLOBALCAMERA.CamFront.Mul(camSpeed))
 	}
 	if window.GetKey(glfw.KeyD) == glfw.Press {
-		globalCamera.CamPosition = globalCamera.CamPosition.Add(mgl32.Vec3.Normalize(globalCamera.CamFront.Cross(mgl32.Vec3{0, 1, 0})).Mul(camSpeed))
+		GLOBALCAMERA.CamPosition = GLOBALCAMERA.CamPosition.Add(mgl32.Vec3.Normalize(GLOBALCAMERA.CamFront.Cross(mgl32.Vec3{0, 1, 0})).Mul(camSpeed))
 	}
 	if window.GetKey(glfw.KeyA) == glfw.Press {
-		globalCamera.CamPosition = globalCamera.CamPosition.Sub(mgl32.Vec3.Normalize(globalCamera.CamFront.Cross(mgl32.Vec3{0, 1, 0})).Mul(camSpeed))
+		GLOBALCAMERA.CamPosition = GLOBALCAMERA.CamPosition.Sub(mgl32.Vec3.Normalize(GLOBALCAMERA.CamFront.Cross(mgl32.Vec3{0, 1, 0})).Mul(camSpeed))
+	}
+	if window.GetMouseButton(glfw.MouseButtonLeft) == glfw.Press && MOUSESTATE == 0 {
+		MOUSESTATE++
+		CHUNKRENDERER.DestroyBlock(&MOUSESTATE)
+	}
+	if window.GetMouseButton(glfw.MouseButtonRight) == glfw.Press && MOUSESTATE == 0 {
+		MOUSESTATE++
+		CHUNKRENDERER.CreateBlock(&MOUSESTATE)
 	}
 }
